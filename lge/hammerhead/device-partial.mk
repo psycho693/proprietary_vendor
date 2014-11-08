23efb40e46bce20a099764e8b88a0252fc3ff5cd
sykopompos <sykopompos@gmail.com>
2014-11-08 00:18:38 -0500

    I kinda double added it

--- lge/hammerhead/device-partial.mk
+++ lge/hammerhead/device-partial.mk
@@ -43,5 +43,5 @@ PRODUCT_COPY_FILES := \
     vendor/lge/hammerhead/proprietary/libvss_nv_core.so:system/vendor/lib/libvss_nv_core.so:lge \
     vendor/lge/hammerhead/proprietary/libvss_nv_idl.so:system/vendor/lib/libvss_nv_idl.so:lge \
     vendor/lge/hammerhead/proprietary/libvss_nv_iface.so:system/vendor/lib/libvss_nv_iface.so:lge \
-    vendor/lge/hammerhead/proprietary/serviceitems.jar:system/framework/serviceitems.jar:qcom \
-    vendor/lge/hammerhead/proprietary/serviceitems.xml:system/etc/permissions/serviceitems.xml:qcom \
+    vendor/lge/hammerhead/proprietary/serviceitems.jar:system/framework/serviceitems.jar:lge \
+    vendor/lge/hammerhead/proprietary/serviceitems.xml:system/etc/permissions/serviceitems.xml:lge 
