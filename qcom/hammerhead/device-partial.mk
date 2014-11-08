b2b0e1ba907f564e9610bf14ab735102a0f5ca9d
sykopompos <sykopompos@gmail.com>
2014-11-08 16:08:20 -0500

    lets see if it will take those missing propritary apps this way along with adding the missing libs into the build

--- qcom/hammerhead/device-partial.mk
+++ qcom/hammerhead/device-partial.mk
@@ -170,4 +170,8 @@ PRODUCT_COPY_FILES := \
     vendor/qcom/hammerhead/proprietary/libsensor_user_cal.so:system/vendor/lib/libsensor_user_cal.so:qcom \
     vendor/qcom/hammerhead/proprietary/libtime_genoff.so:system/vendor/lib/libtime_genoff.so:qcom \
     vendor/qcom/hammerhead/proprietary/libTimeService.so:system/vendor/lib/libTimeService.so:qcom \
+    vendor/qcom/hammerhead/proprietary/librpmb.so:system/vendor/lib/librpmb.so:qcom \
+    vendor/qcom/hammerhead/proprietary/libssd.so:system/vendor/lib/libssd.so:qcom \
+    vendor/qcom/hammerhead/proprietary/shutdownlistener.apk:system/app/shutdownlistener/shutdownlistener.apk:qcom \
+
 
--- qcom/hammerhead/proprietary/Android.mk
+++ qcom/hammerhead/proprietary/Android.mk
@@ -36,18 +36,6 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE := shutdownlistener
-LOCAL_MODULE_TAGS := optional
-LOCAL_BUILT_MODULE_STEM := package.apk
-LOCAL_MODULE_OWNER := qcom
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
-
 # Create symbolic link because user space can access persist directory,
 # while kernel ALSA drivers can only access the /system/etc/firmware directory
 LOCAL_POST_INSTALL_CMD := \
