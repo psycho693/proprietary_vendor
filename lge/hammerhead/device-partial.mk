41a1d2ca598e50af9bb4a331646b6b5d3a93976e
sykopompos <sykopompos@gmail.com>
2014-11-09 03:22:21 -0500

    there goes a little bit more hair.....

--- lge/hammerhead/device-partial.mk
+++ lge/hammerhead/device-partial.mk
@@ -54,6 +54,6 @@ PRODUCT_COPY_FILES := \
     vendor/lge/hammerhead/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so:lge \
     vendor/lge/hammerhead/proprietary/libwvm.so:system/vendor/lib/libwvm.so:lge \
     vendor/lge/hammerhead/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so:lge \
-    vendor/lge/hammerhead/proprietary/libvdmengine.so:system/vendor/lib/libvmengine.so:lge 
+    vendor/lge/hammerhead/proprietary/libvdmengine.so:system/vendor/lib/libvdmengine.so:lge 
    
     
